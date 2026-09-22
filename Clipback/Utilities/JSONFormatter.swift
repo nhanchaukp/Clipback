76b85693073aeeca46bc900de5ca4342726aa7dd
Nhân Châu KP <ctnhan044@gmail.com>
2026-09-22 14:08:45 +0700

    fix: mark JSONFormatter as nonisolated to support background task execution

--- Clipback/Utilities/JSONFormatter.swift
+++ Clipback/Utilities/JSONFormatter.swift
@@ -2,7 +2,7 @@ import Foundation
 import SwiftUI
 
 /// Ultra-lightweight, zero-dependency JSON detector, pretty-formatter, and syntax highlighter
-public enum JSONFormatter {
+nonisolated public enum JSONFormatter {
     
     public struct FormatResult: Sendable {
         public let prettyString: String
